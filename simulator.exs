6fe586a41d879e1c7ec878a7fe19ccf31bbfdb3b
Floris Huetink <florishuetink@gmail.com>
2025-03-15 20:19:06 +0100

    Add rally scoring

--- simulator.exs
+++ simulator.exs
@@ -30,8 +30,103 @@ defmodule Scoring do
       }
     }
   }
-  def traditional(serves) do
-    Enum.reduce(Range.new(1, serves), @state, fn _i, state ->
+
+  defmodule Helpers do
+    def increment_score(state, team) do
+      state
+      |> update_in([:score, team], &(&1 + 1))
+    end
+
+    def switch_positions(state, team) do
+      state
+      |> update_in([:positions, team], fn p -> %{right: p.left, left: p.right} end)
+    end
+
+    def switch_serving_side(%{serving: %{side: :right}} = state) do
+      state
+      |> put_in([:serving, :side], :left)
+    end
+
+    def switch_serving_side(%{serving: %{side: :left}} = state) do
+      state
+      |> put_in([:serving, :side], :right)
+    end
+
+    def award_service(state, team) do
+      state
+      |> put_in([:serving, :team], team)
+    end
+  end
+
+  defmodule Traditional do
+    import Helpers
+
+    def server_won(state, team) do
+      state
+      |> increment_score(team)
+      |> switch_positions(team)
+      |> switch_serving_side()
+    end
+
+    def receiver_won(%{serving: %{server: :first}} = state, _team) do
+      state
+      |> put_in([:serving, :server], :second)
+      |> switch_serving_side()
+    end
+
+    def receiver_won(%{serving: %{server: :second}} = state, team) do
+      state
+      |> award_service(team)
+      |> put_in([:serving, :server], :first)
+      |> put_in([:serving, :side], :right)
+    end
+  end
+
+  defmodule Rally do
+    import Helpers
+    require Integer
+
+    def server_won(state, team) do
+      state
+      |> increment_score(team)
+      |> switch_positions(team)
+      |> switch_serving_side()
+    end
+
+    def receiver_won(state, team) do
+      state
+      |> increment_score(team)
+      |> award_service(team)
+      |> maybe_switch_positions(team)
+      |> put_in([:serving, :side], :right)
+    end
+
+    # defp maybe_switch_positions(
+    #        %{score: %{^team => team_score}, positions: %{^team => %{right: 1, left: 2}}} = state,
+    #        team
+    #      )
+    #      when is_odd(team_score) do
+    #   state
+    #   |> switch_positions(team)
+    # end
+
+    defp maybe_switch_positions(%{score: score, positions: positions} = state, team) do
+      if (Integer.is_odd(score[team]) && positions[team][:right] == 1) ||
+           (Integer.is_even(score[team]) && positions[team][:right] == 2) do
+        state
+        |> switch_positions(team)
+      else
+        state
+      end
+    end
+  end
+
+  def traditional(count), do: run(Traditional, count)
+
+  def rally(count), do: run(Rally, count)
+
+  defp run(module, count) do
+    Enum.reduce(Range.new(1, count), @state, fn _i, state ->
       point_winner = Enum.random([:a, :b])
 
       state =
@@ -42,7 +137,7 @@ defmodule Scoring do
       IO.puts("point winner: #{point_winner}")
 
       state
-      |> point_won(point_winner)
+      |> point_won(point_winner, module)
     end)
   end
 
@@ -54,51 +149,27 @@ defmodule Scoring do
     update_in(state, [:serves, team, player, side], &(&1 + 1))
   end
 
-  defp point_won(%{serving: %{team: :a}} = state, team = :a) do
-    server_won(state, team)
+  defp point_won(%{serving: %{team: :a}} = state, team = :a, module) do
+    module.server_won(state, team)
   end
 
-  defp point_won(%{serving: %{team: :b}} = state, team = :b) do
-    server_won(state, team)
+  defp point_won(%{serving: %{team: :b}} = state, team = :b, module) do
+    module.server_won(state, team)
   end
 
-  defp point_won(%{serving: %{team: :b}} = state, team = :a) do
-    receiver_won(state, team)
+  defp point_won(%{serving: %{team: :b}} = state, team = :a, module) do
+    module.receiver_won(state, team)
   end
 
-  defp point_won(%{serving: %{team: :a}} = state, team = :b) do
-    receiver_won(state, team)
-  end
-
-  defp server_won(state, team) do
-    state
-    |> update_in([:score, team], &(&1 + 1))
-    |> update_in([:positions, team], fn p -> %{right: p.left, left: p.right} end)
-    |> switch_serving_side()
-  end
-
-  defp switch_serving_side(%{serving: %{side: :right}} = state) do
-    state
-    |> put_in([:serving, :side], :left)
-  end
-
-  defp switch_serving_side(%{serving: %{side: :left}} = state) do
-    state
-    |> put_in([:serving, :side], :right)
-  end
-
-  defp receiver_won(%{serving: %{server: :first}} = state, _team) do
-    state
-    |> put_in([:serving, :server], :second)
-    |> switch_serving_side()
-  end
-
-  defp receiver_won(%{serving: %{server: :second}} = state, team) do
-    state
-    |> put_in([:serving, :team], team)
-    |> put_in([:serving, :server], :first)
-    |> put_in([:serving, :side], :right)
+  defp point_won(%{serving: %{team: :a}} = state, team = :b, module) do
+    module.receiver_won(state, team)
   end
 end
 
-Scoring.traditional(1_000_000)
+[type, count] = System.argv()
+count = String.to_integer(count)
+
+case type do
+  "traditional" -> Scoring.traditional(count)
+  "rally" -> Scoring.rally(count)
+end

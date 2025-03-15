a41cae6766eb43d45bb06a00d2b8ec33b8a83c65
Floris Huetink <florishuetink@gmail.com>
2025-03-15 16:01:40 +0100

    Keep serving stats while simulating

--- simulator.exs
+++ simulator.exs
@@ -16,52 +16,89 @@ defmodule Scoring do
     },
     serving: %{
       team: :a,
-      server: :second
+      server: :second,
+      side: :right
+    },
+    serves: %{
+      a: %{
+        1 => %{right: 0, left: 0},
+        2 => %{right: 0, left: 0}
+      },
+      b: %{
+        1 => %{right: 0, left: 0},
+        2 => %{right: 0, left: 0}
+      }
     }
   }
   def traditional(serves) do
-    IO.inspect(@state)
-
-    Enum.reduce(Range.new(1, serves), @state, fn _i, acc ->
+    Enum.reduce(Range.new(1, serves), @state, fn _i, state ->
       point_winner = Enum.random([:a, :b])
-      acc = point_won(point_winner, acc)
 
-      IO.inspect(acc)
+      state =
+        state
+        |> update_serves()
+        |> IO.inspect()
+
+      IO.puts("point winner: #{point_winner}")
+
+      state
+      |> point_won(point_winner)
     end)
   end
 
-  defp point_won(team = :a, %{serving: %{team: :a}} = state) do
-    server_won(team, state)
+  defp update_serves(state) do
+    team = state.serving.team
+    side = state.serving.side
+    player = state.positions[team][side]
+
+    update_in(state, [:serves, team, player, side], &(&1 + 1))
   end
 
-  defp point_won(team = :b, %{serving: %{team: :b}} = state) do
-    server_won(team, state)
+  defp point_won(%{serving: %{team: :a}} = state, team = :a) do
+    server_won(state, team)
   end
 
-  defp point_won(team = :a, %{serving: %{team: :b}} = state) do
-    receiver_won(team, state)
+  defp point_won(%{serving: %{team: :b}} = state, team = :b) do
+    server_won(state, team)
   end
 
-  defp point_won(team = :b, %{serving: %{team: :a}} = state) do
-    receiver_won(team, state)
+  defp point_won(%{serving: %{team: :b}} = state, team = :a) do
+    receiver_won(state, team)
   end
 
-  defp server_won(team, state) do
+  defp point_won(%{serving: %{team: :a}} = state, team = :b) do
+    receiver_won(state, team)
+  end
+
+  defp server_won(state, team) do
     state
     |> update_in([:score, team], &(&1 + 1))
     |> update_in([:positions, team], fn p -> %{right: p.left, left: p.right} end)
+    |> switch_serving_side()
   end
 
-  def receiver_won(_team, %{serving: %{server: :first}} = state) do
+  defp switch_serving_side(%{serving: %{side: :right}} = state) do
+    state
+    |> put_in([:serving, :side], :left)
+  end
+
+  defp switch_serving_side(%{serving: %{side: :left}} = state) do
+    state
+    |> put_in([:serving, :side], :right)
+  end
+
+  defp receiver_won(%{serving: %{server: :first}} = state, _team) do
     state
     |> put_in([:serving, :server], :second)
+    |> switch_serving_side()
   end
 
-  def receiver_won(team, %{serving: %{server: :second}} = state) do
+  defp receiver_won(%{serving: %{server: :second}} = state, team) do
     state
     |> put_in([:serving, :team], team)
     |> put_in([:serving, :server], :first)
+    |> put_in([:serving, :side], :right)
   end
 end
 
-Scoring.traditional(100)
+Scoring.traditional(1_000_000)
